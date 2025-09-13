bd9915308e67fafc7e0dd66ee4395b62813440a4
AldeirAponte <aapontebobadilla@gmail.com>
2025-09-13 18:59:09 -0300

    v2.7

---
+++ src/Enums/GestionarVentas.java
@@ -0,0 +1,5 @@
+package Enums;
+
+public enum GestionarVentas {
+	Nueva, Modificar, Anular, Mostrar_Ventas, Volver
+}
--- src/Usuario/Main.java
+++ src/Usuario/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		int opcionLogin, opcionUsuario, opcionAdmin, opcionAdmin1, opcionAdmin2, opcionAdmin3, opcionVI, opcionVI1,
-				opcionVI2, opcionVI3;
+				opcionVI2, opcionVI3, opcionVenta, opcionVenta1, opcionVenta2;
 
 		// MENU PRINCIPAL
 		do {
@@ -96,15 +96,51 @@ public class Main {
 						break;
 					case 1:
 						// MENU VENDEDOR
-
-						
-						
-						
-						
-						
-						
-						
-						
+						do {
+							opcionVenta = JOptionPane.showOptionDialog(null, "Elija una opción",
+									"Menú Vendedor Local", 0, 1, null, MenuVendedor.values(),
+									MenuVendedor.values());
+							switch (opcionVenta) {
+							case 0: // gestionar clientes
+								do {
+									opcionVenta1 = JOptionPane.showOptionDialog(null, "Elija una opción",
+											"Menú Vendedor Local | Gestion de Clientes", 0, 1, null,
+											GestionarClientes.values(), GestionarClientes.values());
+									switch (opcionVenta1) {
+									case 0: // Modificar o eliminar datos de un cliente
+										JOptionPane.showMessageDialog(null, "Modificar o eliminar datos de un cliente");
+										break;
+									}
+								} while (opcionVenta1 != 1);
+								break;
+							case 1: // gestionar ventas
+								do {
+									opcionVenta2 = JOptionPane.showOptionDialog(null, "Elija una opción",
+											"Menú Vendedor Local | Gestion de Ventas", 0, 1, null,
+											GestionarVentas.values(),
+											GestionarVentas.values());
+									switch (opcionVenta2) {
+									case 0: // nueva venta
+										JOptionPane.showMessageDialog(null, "nueva venta");
+										break;
+									case 1: // modificar venta
+										JOptionPane.showMessageDialog(null, "modificar venta");
+										break;
+									case 2: // anular venta
+										JOptionPane.showMessageDialog(null, "anular venta");
+										break;
+									case 3: // mostrar ventas
+										JOptionPane.showMessageDialog(null, "mostrar ventas");
+										break;
+									}
+								} while (opcionVenta2 != 4);
+								break;
+							case 2:
+								JOptionPane.showMessageDialog(null, "Hasta pronto, Vendedor Local!!");
+								// opcion salir
+								break;
+							}
+						} while (opcionVenta != 2);
 						break;
 					case 2:
 						// MENU VENDEDOR INTERNACIONAL
@@ -173,23 +209,19 @@ public class Main {
 								} while (opcionVI3 != 4);
 								break;
 							case 4:
-								// opcion salir
 								JOptionPane.showMessageDialog(null, "Hasta pronto, Vendedor Internacional!!");
+								// opcion salir
 								break;
 							}
 						} while (opcionVI != 4);
 						break;
-					case 3:
-						break;
 					}
 				} while (opcionUsuario != 3);
 				break;
-			// info
-			case 1:
+			case 1: // info
 				JOptionPane.showMessageDialog(null, "info sobre el programa");
 				break;
-			// salir
-			case 2:
+			case 2: // salir
 				JOptionPane.showMessageDialog(null, "Vuelva pronto!!");
 				break;
 			}
