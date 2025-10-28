da54ae547b6259dc458894590281d5d157ae9f9b
sofiarivass <sofia.rivas@davinci.edu.ar>
2025-10-28 10:46:18 -0300

    v4.6

--- src/UI/Login.java
+++ src/UI/Login.java
@@ -1,22 +1,15 @@
 package UI;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import BLL.Usuario;
-import Repository.Validaciones;
-
 import javax.swing.JTextField;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.Color;
